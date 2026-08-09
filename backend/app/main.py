d9b863fa3d70d713a6ff00aa4812c74c25792d29
Adarsh Menon <adarshmenon.official2007@gmail.com>
2026-08-09 17:52:41 +0530

    Fix CORS for deployed frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,8 @@ app.add_middleware(
 
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://arc-learns.onrender.com"
     ],
 
     allow_credentials=False,
